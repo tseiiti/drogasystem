a767b96b939cde52a1842f63abb29d5091e12093
oscar.taniguchi <oscar.taniguchi@tecnogroup.com.br>
2023-11-30 10:14:38 -0300

    senha ubuntu

--- Aldo/DrogaSystem/conexao.py
+++ Aldo/DrogaSystem/conexao.py
@@ -1,3 +1,3 @@
 import psycopg2
 
-con = psycopg2.connect(host="localhost", database="DrogaSystem", user="postgres", password="827391")
+con = psycopg2.connect(host="localhost", database="drogaSystem", user="postgres", password="postgres")
